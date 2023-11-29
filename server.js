b33e4529c8dbab7a58f57590aeb95d150831fa8e
Omar Masri <omar@Omars-MacBook-Air.local>
2023-11-28 20:40:36 -0500

    setup middleware

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log("hello world...");
+import express from "express";
+const app = express();
+
+import * as dotnev from "dotenv";
+dotnev.config();
+
+import morgan from "morgan";
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+const port = 5100;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.json({ message: "Data Recieved", data: req.body });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
