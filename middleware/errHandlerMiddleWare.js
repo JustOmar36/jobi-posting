256d246cdbd28f2423570a9f644a910542dc5e82
Omar Masri <omar@Omars-MacBook-Air.local>
2023-11-29 21:11:00 -0500

    validation error message fixed

--- middleware/errHandlerMiddleWare.js
+++ middleware/errHandlerMiddleWare.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 
 const errHandlerMiddleWare = (err, req, res, next) => {
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-  const msg = err.msg || "Server Error...Something Went Wrong";
+  const msg = err.message || "Server Error...Something Went Wrong";
 
   res.status(statusCode).json({ msg: msg });
 };
